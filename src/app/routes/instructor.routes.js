2ff9e810a430623d16dd9d08b4c55f3db1de6135
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-08-25 12:29:42 -0300

    Criando e implementando as rotas do instructor

---
+++ src/app/routes/instructor.routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const route = express.Router()
+const Instructor = require('./../controllers/instructor.controller')
+
+route.post('/create', Instructor.createInstructor)
+route.get('/viewAll', Instructor.viewAllInstructor)
+route.get('/viewOne/:name', Instructor.viewOneInstructor)
+route.put('/updateOne/:name', Instructor.updateOneInstructor)
+route.delete('/deleteOne/:name', Instructor.deleteOneInstructor)
+
+module.exports = route
